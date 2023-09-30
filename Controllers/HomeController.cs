7ad6b53de16a80a5964805927c032e27f7dd780d
Jose Luis Encarnacion <jose.lencarnacion1@gmail.com>
2023-09-30 00:50:06 -0400

    Initial project architecture

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Practica2MCV.Models;
+
+namespace Practica2MCV.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult SobreMi()
+    {
+        return View();
+    }
+
+     public IActionResult Patrones()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Views/Home/Patrones.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class PatronesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
